7071cbf3b14867b4c20977ea623201e1764be0f4
Samuel Laferriere <samlaf92@gmail.com>
2022-04-25 16:23:14 +0200

    block hash working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 We use getblock to get blockhash, and then query the actual block. See https://getblock.io/docs/available-nodes-methods/BTC/JSON-RPC/getblockhash/
+
+See https://en.bitcoin.it/wiki/Block_hashing_algorithm for block hashing algorithm
--- lightclient.py
+++ lightclient.py
@@ -1,5 +1,6 @@
 import json
 from hashlib import sha256
+from binascii import unhexlify
 
 with open("block0.json") as block0_file:
     block0 = json.load(block0_file)
@@ -10,28 +11,56 @@ with open("block1.json") as block1_file:
 # [version, previousblockhash, merkleroot, time, bits, nonce]
 # [4,       32,                32,         4,    4,    4    ]
 # [int,     string,            string,     int,  string, int]
-# all little endian (??)
+# all little endian
 
 ENDIANNESS = 'little'
 
 
+def to_bytes(string, unhexlify=True):
+    if not string:
+        return b''
+    if unhexlify:
+        try:
+            if isinstance(string, bytes):
+                string = string.decode()
+            s = bytes.fromhex(string)
+            return s
+        except (TypeError, ValueError):
+            pass
+    if isinstance(string, bytes):
+        return string
+    else:
+        return bytes(string, 'utf8')
+
+
+def double_sha256(string, as_hex=False):
+    if not as_hex:
+        return sha256(sha256(string).digest()).digest()
+    else:
+        return sha256(sha256(string).digest()).hexdigest()
+
+
+def big_to_little_endian(s):
+    return bytes.fromhex(s)[::-1].hex()
+
+
 def verifyBlock(block):
-    versionB = block['version'].to_bytes(4, ENDIANNESS)
-    previousBlockHashB = bytes.fromhex(
-        block['previousBlock']) if 'previousBlock' in block else (0).to_bytes(32, ENDIANNESS)
-    merkleRootB = bytes.fromhex(block['merkleroot'])
-    timeB = block['time'].to_bytes(4, ENDIANNESS)
-    bitsB = bytes.fromhex(block['bits'])
-    nonceB = block['nonce'].to_bytes(4, ENDIANNESS)
+    versionHex = big_to_little_endian(block['versionHex'])
+    previousBlockHashHex = big_to_little_endian(
+        block['previousblockhash']) if 'previousblockhash' in block else (0).to_bytes(32, ENDIANNESS).hex()
+    merkleRootHex = big_to_little_endian(block['merkleroot'])
+    timeHex = block['time'].to_bytes(4, ENDIANNESS).hex()
+    bitsHex = big_to_little_endian(block['bits'])
+    nonceB = block['nonce'].to_bytes(4, ENDIANNESS).hex()
 
-    blockHeaderB = versionB + previousBlockHashB + \
-        merkleRootB + timeB + bitsB + nonceB
-    print(sha256(bytes.fromhex(sha256(blockHeaderB).hexdigest())).hexdigest())
-    print(block['hash'])
-
-
-def verify(prevblock, curblock):
-    curblock.previousblockhash == prevblock.hash
+    header_hex = versionHex + previousBlockHashHex + \
+        merkleRootHex + timeHex + bitsHex + nonceB
+    header_bin = unhexlify(header_hex)
+    hash = sha256(sha256(header_bin).digest()).digest()
+    print(hash[::-1].hex())
 
 
+verifyBlock(block0)
+print(block0['hash'])
 verifyBlock(block1)
+print(block1['hash'])
